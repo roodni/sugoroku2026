53accc3c966c99dc1e178fea13eda810428cd062
roodni <41773436+roodni@users.noreply.github.com>
2025-12-28 18:16:32 +0900

    インテントリンクを修正

--- src/app/Goaled.tsx
+++ src/app/Goaled.tsx
@@ -4,11 +4,9 @@ const HASHTAG = "sugoroku2026";
 const INTENT_BASE = "https://twitter.com/intent/tweet";
 const HERE_URL = location.href;
 
-function tweetUrl(text: string, url: string, hashtags: string[]) {
+function tweetUrl(text: string) {
   const params = new URLSearchParams();
   params.set("text", text);
-  params.set("hashtags", hashtags.join(","));
-  params.set("url", url);
   return `${INTENT_BASE}?${params.toString()}`;
 }
 
@@ -17,21 +15,17 @@ export const Goaled: React.FC<{
   restartGame: () => void;
 }> = ({ getGameState, restartGame }) => {
   const gameState = getGameState();
-  const text = gameState.gameOverMessage + "\n";
-
-  if (text === null) {
+  if (gameState.gameOverMessage === null) {
     return undefined;
   }
 
-  const url = HERE_URL;
-  const allText = `${text}${HERE_URL} #${HASHTAG}`;
-
+  const text = `${gameState.gameOverMessage}\n${HERE_URL} #${HASHTAG}`;
   return (
     <div className="goaled">
-      <textarea className="goaled-textarea" readOnly value={allText} />
+      <textarea className="goaled-textarea" readOnly value={text} />
       <div className="goaled-buttons">
         <div>
-          <a href={tweetUrl(text, url, [HASHTAG])} target="_blank">
+          <a href={tweetUrl(text)} target="_blank">
             𝕏 (タブが開きます)
           </a>
         </div>
